e54c25b63f3d1382fdccf7f9688b7661d95cf345
ReaLLLxStyle <145691510+ReaLLLxStyle@users.noreply.github.com>
2023-11-20 12:30:24 +0800

    Add files via upload

---
+++ LoginandSignup/src/main/java/com/mycompany/loginandsignup/LoginandSignup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginandsignup;
+
+/**
+ *
+ * @author JCTECH
+ */
+public class LoginandSignup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
